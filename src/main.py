a9037ad2adc8ddba614c4341946389cd97ca3433
Aleksandr6669 <50256477+Aleksandr6669@users.noreply.github.com>
2025-03-30 11:32:45 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 import flet as ft
-from flet_camera import Camera
+import os
 
 def main(page: ft.Page):
-    cam = Camera()
-    page.add(cam)
+    page.title = "Камера в WebView"
 
+    # Путь к HTML-файлу с камерой
+    html_file_path = os.path.join(os.path.dirname(__file__), "camera_extension", "camera.html")
+
+    # Чтение HTML-файла
+    with open(html_file_path, "r", encoding="utf-8") as file:
+        camera_html = file.read()
+
+    # Добавляем WebView для отображения камеры
+    page.add(ft.WebView(content=camera_html))
+
+# Запускаем приложение
 ft.app(target=main)
