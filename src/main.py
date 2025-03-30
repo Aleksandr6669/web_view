aa84f9142c157c910fb773782da58d376aad0560
Aleksandr6669 <50256477+Aleksandr6669@users.noreply.github.com>
2025-03-30 17:06:47 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,18 +1,9 @@
 import flet as ft
-import cv2
 
 def main(page: ft.Page):
-    def capture_camera(e):
-        cap = cv2.VideoCapture(0)  # Открываем доступ к камере
-        ret, frame = cap.read()  # Считываем один кадр
-        if ret:
-            cv2.imwrite("snapshot.png", frame)  # Сохраняем снимок в файл
-            page.add(ft.Text("Снимок сохранён как 'snapshot.png'."))
-        else:
-            page.add(ft.Text("Не удалось сделать снимок."))
-        cap.release()
-
-    page.title = "Камера на Flet"
-    page.add(ft.TextButton("Сделать снимок", on_click=capture_camera))
+    page.title = "Старт"
+    
+    # Указываем URL загруженного HTML-файла
+    page.add(ft.WebView(src="https://tmbot.kivismart.com/webapp_index/353095791"))
 
 ft.app(target=main)
