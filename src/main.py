b9de1871b2a55b792236aab05ce1e4da37192c41
Aleksandr6669 <50256477+Aleksandr6669@users.noreply.github.com>
2025-03-30 12:19:49 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -31,7 +31,7 @@ camera_html = """
             canvas.height = video.videoHeight;
             ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
             let imageData = canvas.toDataURL('image/png');
-            // Отправка данных родителю
+            // Отправка данных в Flet через window.postMessage
             window.parent.postMessage(imageData, "*");
         };
     </script>
@@ -42,22 +42,24 @@ camera_html = """
 def main(page: ft.Page):
     page.title = "Камера на Flet"
 
-    def on_message(data: str):
-        # Получаем изображение в формате base64
-        print("Фото получено:", data)
-        img.src_base64 = data  # Отображаем снимок
-        page.update()
-
     # Кодируем HTML в data URL
     html_data_url = f"data:text/html;charset=utf-8,{camera_html}"
-
-    # Создаем WebView с встроенным HTML и передаем callback для получения данных
-    web_view = ft.WebView(url=html_data_url, on_message=on_message)
 
     # Виджет для отображения снимка
     img = ft.Image()
 
-    # Добавляем WebView и изображение на страницу
-    page.add(web_view, img)
+    # Создаем WebView с встроенным HTML
+    web_view = ft.WebView(url=html_data_url)
+
+    # Функция для получения данных изображения
+    def get_image_from_webview(e):
+        page.add(img)  # Отобразим изображение на странице
+        img.src_base64 = e.message  # Устанавливаем полученное изображение
+        page.update()
+
+    # Регистрируем обработчик сообщений из WebView
+    web_view.on_message = get_image_from_webview
+
+    page.add(web_view)
 
 ft.app(target=main, view=ft.WEB_BROWSER)
